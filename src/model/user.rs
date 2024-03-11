a0f4ca3fb76b1872d96c239702a084efc04c4efe
Camille Cutrone <cjcutroneiii@gmail.com>
2024-03-11 09:19:50 -0500

    build(cli): add excel user model

---
+++ src/model/user.rs
@@ -0,0 +1,17 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    pub first_name: String,
+    pub last_name: String,
+    #[serde(deserialize_with = "deserialize_string_as_vec")]
+    pub groups: Vec<String>,
+}
+
+fn deserialize_string_as_vec<'de, D>(deserializer: D) -> Result<Vec<String>, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    Ok(s.split(',').map(|s| s.trim().to_string()).collect())
+}
