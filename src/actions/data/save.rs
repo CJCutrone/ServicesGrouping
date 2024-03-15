a4cbec1712ec956b1b1a4ce5091f038f63a12a70
Camille Cutrone <cjcutroneiii@gmail.com>
2024-03-15 14:19:32 -0500

    feat(cli): upsert users instead of inserting

--- src/actions/data/save.rs
+++ src/actions/data/save.rs
@@ -1,11 +1,16 @@
-use diesel::{insert_into, PgConnection, RunQueryDsl};
+use diesel::{ExpressionMethods, insert_into, PgConnection, RunQueryDsl};
+use diesel::upsert::excluded;
 
 use crate::model::database::User;
 use crate::schema::users;
+use crate::schema::users::{id, planning_center_id};
 
 pub fn to_database(conn: &mut PgConnection, data: &Vec<User>) {
     insert_into(users::table)
         .values(data)
+        .on_conflict(id)
+        .do_update()
+        .set(planning_center_id.eq(excluded(planning_center_id)))
         .execute(conn)
         .expect("Error inserting users");
 }
