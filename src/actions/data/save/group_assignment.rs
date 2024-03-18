4ccb233bfd6238ef8a6c839a3e0bf2099c35532d
Camille Cutrone <cjcutroneiii@gmail.com>
2024-03-18 09:35:32 -0500

    feat(cli): add GroupAssignment actions

---
+++ src/actions/data/save/group_assignment.rs
@@ -0,0 +1,19 @@
+use diesel::{ExpressionMethods, insert_into, PgConnection, RunQueryDsl};
+use diesel::upsert::excluded;
+
+use crate::model::database::{GroupAssignment};
+use crate::schema::{group_assignments};
+use crate::schema::group_assignments::{id, group_id, user_id};
+
+pub fn to_database(conn: &mut PgConnection, data: &Vec<GroupAssignment>) {
+    insert_into(group_assignments::table)
+        .values(data)
+        .on_conflict(id)
+        .do_update()
+        .set((
+            group_id.eq(excluded(group_id)),
+            user_id.eq(excluded(user_id))
+        ))
+        .execute(conn)
+        .expect("Error inserting group assignments");
+}
--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,16 @@ use std::env;
 
 use diesel::{Connection, PgConnection};
 use dotenv::dotenv;
-
-use crate::model::database::{Group};
-
 pub mod model;
 pub mod actions;
 pub mod schema;
 
 fn main() {
-    let path = "C:\\Users\\CJ\\Downloads\\test_groups.json";
-    let data = actions::data::load::group::from_json(path);
-
-    //convert excel User to database User
-    let data = data.iter().map(|group|
-        Group::from_json(group)
-    ).collect();
-
+    let path = "C:\\Users\\CJ\\Downloads\\test.xlsx";
+    let data = actions::data::load::group_assignment::from(path);
 
     let mut connection = establish_connection();
-
-    actions::data::save::group::to_database(&mut connection, &data);
+    actions::data::save::group_assignment::to_database(&mut connection, &data);
 }
 
 pub fn establish_connection() -> PgConnection {
